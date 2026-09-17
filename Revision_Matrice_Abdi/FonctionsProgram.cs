4fc3bff6a4be87b09be2491da22076500cd8c46e
abdirahman220923 <220923@site.asty-moulin.be>
2026-09-17 20:52:29 +0200

    Add files via upload

---
+++ Revision_Matrice_Abdi/FonctionsProgram.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Revision_Matrice_Abdi
+{
+    internal class FonctionsProgram
+    {
+        public static void LireEntier(string question, out int resultat)
+        {
+            do
+            {
+                Console.WriteLine(question);
+            }
+            while (!int.TryParse(Console.ReadLine(), out resultat));
+        }
+        public static void ChiffrerVigenere(string message, string cle, out string[,] matrice)
+        {
+            int nbColonnes = message.Length;
+            matrice = new string[4, nbColonnes];
+
+            int iColonne = 0;
+            int iCle = 0;
+            while (iColonne < nbColonnes)
+            {
+                matrice[0, iColonne] = message[iColonne].ToString();
+                matrice[1, iColonne] = cle[iCle].ToString();
+                int codeClair = (int)message[iColonne];
+                int codeCle = (int)cle[iCle];
+                iCle = iCle + 1;
+                if (iCle >= cle.Length)
+                {
+                    iCle = 0;
+                }
+                int decalage = codeCle - 65;
+                matrice[2, iColonne] = Convert.ToString(decalage);
+                int codeChiffre = ((codeClair - 65) + decalage) % 26;
+                matrice[3, iColonne] = Convert.ToChar(codeChiffre + 65).ToString();
+
+                iColonne = iColonne + 1;
+            }
+        }
+        public static void ChiffrerAffine(string message, int a, int b, out string[,] matrice)
+        {
+            int nbColonnes = message.Length;
+            matrice = new string[4, nbColonnes];
+
+            int iColonne = 0;
+            while (iColonne < nbColonnes)
+            {
+                matrice[0, iColonne] = message[iColonne].ToString();
+
+                int x = (int)message[iColonne] - 65;
+                matrice[1, iColonne] = Convert.ToString(x);
+
+                int y = (a * x + b) % 26;
+                matrice[2, iColonne] = Convert.ToString(y);
+
+                matrice[3, iColonne] = Convert.ToChar(y + 65).ToString();
+
+                iColonne = iColonne + 1;
+            }
+        }
+        public static void AfficherMatrice(char[,] matrice, int nbLignes, int nbColonnes)
+        {
+            int iLigne = 0;
+            while (iLigne < nbLignes)
+            {
+                string chaine = "";
+                int iColonne = 0;
+                while (iColonne < nbColonnes)
+                {
+                    chaine = chaine + matrice[iLigne, iColonne] + " ";
+                    iColonne = iColonne + 1;
+                }
+                Console.WriteLine(chaine);
+                iLigne = iLigne + 1;
+            }
+        }
+        public static void NettoyerMessage(string messageBrut, out string messagePropre)
+        {
+            messagePropre = "";
+            int iCaractere = 0;
+            while (iCaractere < messageBrut.Length)
+            {
+                char c = messageBrut[iCaractere];
+                if (c != ' ')
+                {
+                    if (c >= 'a' && c <= 'z')
+                    {
+                        c = (char)(c - 32);
+                    }
+                    messagePropre = messagePropre + c;
+                }
+                iCaractere = iCaractere + 1;
+            }
+        }
+        public static void VerifierA(int a, out bool valide)
+        {
+            valide = false;
+            if (a == 1 || a == 3 || a == 5 || a == 7 || a == 9 || a == 11)
+            {
+                valide = true;
+            }
+            if (a == 15 || a == 17 || a == 19 || a == 21 || a == 23 || a == 25)
+            {
+                valide = true;
+            }
+        }
+    }
+}
---
+++ Revision_Matrice_Abdi/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Revision_Matrice_Abdi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string messageBrut;
+            string messagePropre;
+            string cle;
+            int a;
+            int b;
+            int choix;
+            string recommencer;
+            string[,] matrice;
+            bool valide;
+
+            Console.WriteLine("BienvenueBienvenue dans l'application de cryptage");
+
+            do
+            {
+                Console.WriteLine("Choisissez une méthode de cryptage :");
+                Console.WriteLine("1 = Chiffre de Vigenère");
+                Console.WriteLine("2 = Chiffre Affine");
+                FonctionsProgram.LireEntier("Votre choix : ", out choix);
+
+                if (choix == 1)
+                {
+                    Console.WriteLine("Entrez votre message :");
+                    messageBrut = Console.ReadLine();
+                    FonctionsProgram.NettoyerMessage(messageBrut, out messagePropre);
+
+                    Console.WriteLine("Entrez la clé :");
+                    cle = Console.ReadLine();
+
+                    FonctionsProgram.ChiffrerVigenere(messagePropre, cle, out matrice);
+
+                    Console.WriteLine(" MATRICE DE CRYPTAGE ");
+                    FonctionsProgram.AfficherMatrice(matrice, 4, messagePropre.Length);
+
+                    Console.WriteLine("Message chiffré : ");
+                    int iColonne = 0;
+                    while (iColonne < messagePropre.Length)
+                    {
+                        Console.WriteLine(matrice[3, iColonne]);
+                        iColonne = iColonne + 1;
+                    }
+                    Console.WriteLine();
+                }
+                else if (choix == 2)
+                {
+                    Console.WriteLine("Entrez votre message :");
+                    messageBrut = Console.ReadLine();
+                    FonctionsProgram.NettoyerMessage(messageBrut, out messagePropre);
+
+                    do
+                    {
+                        FonctionsProgram.LireEntier("Entrez a (valeurs possibles : 1,3,5,7,9,11,15,17,19,21,23,25) :", out a);
+                        FonctionsProgram.VerifierA(a, out valide);
+                        if (valide == false)
+                        {
+                            Console.WriteLine("Valeur de a invalide ! Recommencez.");
+                        }
+                    }
+                    while (valide == false);
+
+                    do
+                    {
+                        FonctionsProgram.LireEntier("Entrez b (entre 0 et 25) :", out b);
+                        if (b < 0 || b > 25)
+                        {
+                            Console.WriteLine("Valeur de b invalide ! Recommencez.");
+                        }
+                    }
+                    while (b < 0 || b > 25);
+                    FonctionsProgram.ChiffrerAffine(messagePropre, a, b, out matrice);
+
+                    Console.WriteLine("=== MATRICE DE CRYPTAGE ===");
+                    FonctionsProgram.AfficherMatrice(matrice, 4, messagePropre.Length);
+
+                    Console.WriteLine ("Message chiffré : ");
+                    int iColonne = 0;
+                    while (iColonne < messagePropre.Length)
+                    {
+                        Console.WriteLine(matrice[3, iColonne]);
+                        iColonne = iColonne + 1;
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Choix invalide !");
+                }
+
+
+                Console.WriteLine("Voulez-vous recommencer ? (espace = oui / autre = non)");
+                recommencer = Console.ReadLine();
+            } while (recommencer == " ");
+        }
+    }
+}
